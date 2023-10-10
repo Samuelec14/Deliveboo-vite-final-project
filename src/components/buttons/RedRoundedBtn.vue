f97a6ed08b16824978e7cf5d5159dbd898ab6a91
Samuelec14 <samuconf14@gmail.com>
2023-10-10 15:17:01 +0200

    adding button components

---
+++ src/components/buttons/RedRoundedBtn.vue
@@ -0,0 +1,47 @@
+<script>
+
+export default {
+    name: "RedRoundedBtn",
+
+    data() {
+    return{
+
+    }
+    },
+    methods: {
+
+    }
+}
+</script>
+
+<template>
+
+    <button  id="btn-1" type="button" class="text-light mx-3 btn btn-warning border border-dark px-3"><strong>Button</strong></button>
+    <!-- <button id="btn-2" type="button" class="btn btn-warning border border-dark px-3"><strong>Button</strong></button> -->
+
+</template>
+
+<style lang="scss" scoped>
+
+button{
+    background-color: #db320f;
+    transition: all 0.25s ease;
+}
+
+#btn-1:hover {    
+    transform: translateY(-3px);
+    background-color: #db320f;
+    // color: white;
+    border: 1px solid #db320f;
+
+}
+
+// #btn-2:hover {    
+//     // transform: translateY(-3px);
+//     background-color: rgba(0, 0, 0, 0.904);
+//     color: #ffbe23;
+//     border: 1px solid #ffbe23;
+
+// }
+
+</style>
---
+++ src/components/buttons/YellowRoundedBtn.vue
@@ -0,0 +1,47 @@
+<script>
+
+export default {
+    name: "YellowRoundedBtn",
+
+    data() {
+    return{
+
+    }
+    },
+    methods: {
+
+    }
+}
+</script>
+
+<template>
+
+    <button  id="btn-1" type="button" class="btn btn-warning border border-dark px-3 mx-2"><strong>Button</strong></button>
+    <button id="btn-2" type="button" class="btn btn-warning border border-dark px-3"><strong>Button</strong></button>
+
+</template>
+
+<style lang="scss" scoped>
+
+button{
+    background-color: #ffbe23;
+    transition: all 0.25s ease;
+}
+
+#btn-1:hover {    
+    transform: translateY(-3px);
+    // background-color: #ffbe23;
+    // color: black;
+    border: 1px solid #ffbe23;
+    // text-shadow: -1px 0 black, 0 1px black, 1px 0 black, 0 -1px black
+}
+
+#btn-2:hover {    
+    // transform: translateY(-3px);
+    background-color: rgba(0, 0, 0, 0.904);
+    // color: #ffbe23;
+    border: 1px solid #ffbe23;
+
+}
+
+</style>
