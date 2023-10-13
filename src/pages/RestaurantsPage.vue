dd0f56e61dd5e14b81c8019b0de84cf219ea1a65
MembriniLuca29 <lucamembrini29@gmail.com>
2023-10-13 17:03:52 +0200

    .

--- src/pages/RestaurantsPage.vue
+++ src/pages/RestaurantsPage.vue
@@ -3,9 +3,6 @@ import HeaderComponent from '../components/HeaderComponent.vue';
 import MainComponent from '../components/MainComponent.vue';
 import FooterComponent from '../components/FooterComponent.vue';
 import axios from 'axios';
-import { store } from '../store';
-
-
 
 export default {
     components: {
@@ -45,41 +42,35 @@ export default {
 </script>
 
 <template>
-
-    <HeaderComponent></HeaderComponent>
-    <h2 class="text-center my-4">I PIÙ VICINI A TE</h2>
-    <div class="container d-flex flex-wrap">
-      
-    <div v-for="restaurant in restaurants" :key="restaurant.id" class="card m-2" style="width: 18rem;">
-      <img :src="restaurant.thumb" class="card-img-top" alt="...">
-      <div class="card-body">
-        <h2 class="card-title">{{ restaurant.name }}</h2>
-        <h4 class="card-text">tipologie del ristorante</h4>
-        <p class="card-text">{{ restaurant.address }}</p>
-        <h3>{{ restaurant.phone_number }}</h3>
-      </div>
-    </div>
-</div>
-
-<div class="container">
-  <h3 class="text-center my-4">SCEGLI PER TIPOLOGIA</h3>
-  <div class="container d-flex flex-wrap justify-content-center mb-4">
-      
-      <div v-for="type in types" :key="type.id" class="card m-2" style="width: 18rem;" >  
-        <img :src="type.thumb" class="card-img-top" alt="...">
-        <div class="card-body">
-          <h2 class="card-title">{{ type.name }}</h2>
-          <h4>vedi i più vicini a te </h4>
+    <div>
+        <HeaderComponent></HeaderComponent>
+        <h2 class="text-center my-4">I PIÙ VICINI A TE</h2>
+        <div class="container d-flex flex-wrap">
+            <div v-for="restaurant in restaurants" :key="restaurant.id" class="card m-2" style="width: 18rem;" v-if="restaurants.length > 0" @click="navigateToDish(restaurant.id)">
+                <img :src="restaurant.thumb" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h2 class="card-title">{{ restaurant.name }}</h2>
+                    <h4 class="card-text">tipologie del ristorante</h4>
+                    <p class="card-text">{{ restaurant.address }}</p>
+                    <h3 class="phone-number p-3" @click.stop="navigateToDish(restaurant.id)">{{ restaurant.phone_number }}</h3>
+                </div>
+            </div>
         </div>
-      </div>
-  </div>
-</div>
-    <FooterComponent></FooterComponent>
-        
-       
-        
 
-
+        <div class="container">
+            <h3 class="text-center my-4">SCEGLI PER TIPOLOGIA</h3>
+            <div class="container d-flex flex-wrap justify-content-center mb-4">
+                <div v-for="type in types" :key="type.id" class="card m-2" style="width: 18rem;" v-if="types.length > 0">
+                    <img :src="type.thumb" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h2 class="card-title">{{ type.name }}</h2>
+                        <h4>vedi i più vicini a te</h4>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <FooterComponent></FooterComponent>
+    </div>
 </template>
 
 <style scoped lang="scss">
