dfd38199b9ee10049f646d29b82749606a57ef8b
MembriniLuca29 <lucamembrini29@gmail.com>
2023-10-13 16:56:06 +0200

    dishfilter-in progress

--- src/pages/RestaurantsPage.vue
+++ src/pages/RestaurantsPage.vue
@@ -16,29 +16,27 @@ export default {
             types: [],
         };
     },
-    mounted() {
-        this.fetchRestaurants();
-        this.fetchTypes();
-    },
     methods: {
-        fetchRestaurants() {
-            axios.get('http://127.0.0.1:8000/api/restaurant/restaurant')
-                .then(response => {
-                    this.restaurants = response.data.results;
-                })
-                .catch(error => {
-                    console.error(error);
-                });
+        async fetchData() {
+            try {
+                const [restaurantsResponse, typesResponse] = await Promise.all([
+                    axios.get('http://127.0.0.1:8000/api/restaurant/restaurant'),
+                    axios.get('http://127.0.0.1:8000/api/type/type')
+                ]);
+
+                this.restaurants = restaurantsResponse.data.results;
+                this.types = typesResponse.data.results;
+            } catch (error) {
+                console.error(error);
+            }
         },
-        fetchTypes() {
-            axios.get('http://127.0.0.1:8000/api/type/type')
-                .then(response => {
-                    this.types = response.data.results;
-                })
-                .catch(error => {
-                    console.error(error);
-                });
-        },
+        navigateToDish(restaurantId) {
+    this.$router.push({ name: 'dish', params: { restaurant_id: restaurantId } });
+},
+
+    },
+    mounted() {
+        this.fetchData();
     },
 };
 </script>
@@ -48,14 +46,13 @@ export default {
         <HeaderComponent></HeaderComponent>
         <h2 class="text-center my-4">I PIÙ VICINI A TE</h2>
         <div class="container d-flex flex-wrap">
-            <div v-for="restaurant in restaurants" :key="restaurant.id" class="card m-2" style="width: 18rem;" v-if="restaurants.length > 0">
-                <!-- Aggiunto v-if per verificare che restaurants sia definito -->
+            <div v-for="restaurant in restaurants" :key="restaurant.id" class="card m-2" style="width: 18rem;" v-if="restaurants.length > 0" @click="navigateToDish(restaurant.id)">
                 <img :src="restaurant.thumb" class="card-img-top" alt="...">
                 <div class="card-body">
                     <h2 class="card-title">{{ restaurant.name }}</h2>
                     <h4 class="card-text">tipologie del ristorante</h4>
                     <p class="card-text">{{ restaurant.address }}</p>
-                    <h3>{{ restaurant.phone_number }}</h3>
+                    <h3 class="phone-number p-3" @click.stop="navigateToDish(restaurant.id)">{{ restaurant.phone_number }}</h3>
                 </div>
             </div>
         </div>
@@ -64,7 +61,6 @@ export default {
             <h3 class="text-center my-4">SCEGLI PER TIPOLOGIA</h3>
             <div class="container d-flex flex-wrap justify-content-center mb-4">
                 <div v-for="type in types" :key="type.id" class="card m-2" style="width: 18rem;" v-if="types.length > 0">
-                    <!-- Aggiunto v-if per verificare che types sia definito -->
                     <img :src="type.thumb" class="card-img-top" alt="...">
                     <div class="card-body">
                         <h2 class="card-title">{{ type.name }}</h2>
@@ -78,5 +74,11 @@ export default {
 </template>
 
 <style scoped lang="scss">
+.card {
+    cursor: pointer;
+}
 
+.phone-number {
+    color: blue;
+}
 </style>
