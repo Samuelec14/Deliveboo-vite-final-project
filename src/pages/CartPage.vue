090c271eed50a1b65a358562f2b4ffd6c4d78e9e
MembriniLuca29 <lucamembrini29@gmail.com>
2023-10-16 16:27:31 +0200

    .

--- src/pages/CartPage.vue
+++ src/pages/CartPage.vue
@@ -3,12 +3,7 @@ import HeaderComponent from '../components/HeaderComponent.vue';
 import FooterComponent from '../components/FooterComponent.vue';
 import { store } from '../store';
 export default {
-    props: {
-    dish: {
-        type: Object,
-        required: true
-    }
-},
+   
     components: {
         HeaderComponent,
         FooterComponent,
