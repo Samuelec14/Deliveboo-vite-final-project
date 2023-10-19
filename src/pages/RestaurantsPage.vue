a69e8c4360285e3759775fa2bee294c209906565
Samuelec14 <samuconf14@gmail.com>
2023-10-19 15:08:53 +0200

    some indentation

--- src/pages/RestaurantsPage.vue
+++ src/pages/RestaurantsPage.vue
@@ -79,51 +79,51 @@ export default {
 </script>
 
 <template>
-  <div>
-    <!-- Header Component -->
-    <HeaderComponent></HeaderComponent>
+<div>
+<!-- Header Component -->
+<HeaderComponent></HeaderComponent>
 
-    <!-- Searchbar Component -->
-    <Searchbar @value="getValue" />
+<!-- Searchbar Component -->
+<Searchbar @value="getValue" />
 
-    <div class="container">
-      <div class="row">
+<div class="container">
+    <div class="row">
 
-        <!-- Print Restaurants by Type -->
-        <div v-for="restaurant in restaurants" :key="restaurant.id" class="col-12 col-md-6 col-lg-4 my-3">
-          <div class="card" style="width: 100%;" @click="navigateToDish(restaurant.id)">
-            <img :src="store.imgPath + restaurant.thumb" class="card-img-top" alt="Restaurant Image">
-            <div class="card-body">
-              <h2 class="card-title">{{ restaurant.name }}</h2>
-              <h4 class="card-text">Tipologie del ristorante</h4>
-              <p class="card-text">{{ restaurant.address }}</p>
-              <h3 class="phone-number p-3">{{ restaurant.phone_number }}</h3>
-            </div>
-          </div>
+    <!-- Print Restaurants by Type -->
+    <div v-for="restaurant in restaurants" :key="restaurant.id" class="col-12 col-md-6 col-lg-4 my-3">
+        <div class="card" style="width: 100%;" @click="navigateToDish(restaurant.id)">
+        <img :src="store.imgPath + restaurant.thumb" class="card-img-top" alt="Restaurant Image">
+        <div class="card-body">
+            <h2 class="card-title">{{ restaurant.name }}</h2>
+            <h4 class="card-text">Tipologie del ristorante</h4>
+            <p class="card-text">{{ restaurant.address }}</p>
+            <h3 class="phone-number p-3">{{ restaurant.phone_number }}</h3>
         </div>
-
-        <!-- Error Message -->
-        <div v-if="restaurants.length === 0 && !loading" class="col-12 text-center">
-          <h2>Nessun ristorante corrisponde a questa tipologia</h2>
         </div>
-      </div>
     </div>
 
-    <div class="container">
-      <h3 class="text-center my-4">SCEGLI PER TIPOLOGIA</h3>
-      <div class="row">
-        <!-- Display Types -->
-        <div v-for="type in types" :key="type.id" @click="fetchRestaurants(type.name); scrollToTop();" class="col-12 col-md-6 col-lg-4 my-3">
-          <div class="card" style="width: 100%;">
-            <img :src="store.imgPath + type.thumb" class="card-img-top" :alt="type.name">
-            <div class="card-body">
-              <h2 class="card-title">{{ type.name }}</h2>
-              <h4>vedi i più vicini a te</h4>
-            </div>
-          </div>
-        </div>
-      </div>
+    <!-- Error Message -->
+    <div v-if="restaurants.length === 0 && !loading" class="col-12 text-center">
+        <h2>Nessun ristorante corrisponde a questa tipologia</h2>
     </div>
+    </div>
+</div>
+
+<div class="container">
+    <h3 class="text-center my-4">SCEGLI PER TIPOLOGIA</h3>
+    <div class="row">
+    <!-- Display Types -->
+    <div v-for="type in types" :key="type.id" @click="fetchRestaurants(type.name); scrollToTop();" class="col-12 col-md-6 col-lg-4 my-3">
+        <div class="card" style="width: 100%;">
+        <img :src="store.imgPath + type.thumb" class="card-img-top" :alt="type.name">
+        <div class="card-body">
+            <h2 class="card-title">{{ type.name }}</h2>
+            <h4>vedi i più vicini a te</h4>
+        </div>
+        </div>
+    </div>
+    </div>
+</div>
 
     <!-- Footer Component -->
     <FooterComponent></FooterComponent>
