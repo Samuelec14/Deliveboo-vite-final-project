e4255d6e9814b15e7344e73f9a217232030f43e8
Alessiovinci87 <alessio.vinci87@gmail.com>
2023-10-29 13:24:20 +0100

    UsPage completata

--- src/pages/UsPage.vue
+++ src/pages/UsPage.vue
@@ -13,13 +13,19 @@ export default {
   components: {
     HeaderComponent,
     FooterComponent,
-    FooterComponent
 },
 }
 </script>
 
 <template>
 <HeaderComponent></HeaderComponent>
+
+<div class="about-section">
+
+    <h2>Chi siamo</h2>
+    <p>In un mondo in continua evoluzione digitale, abbiamo riconosciuto la necessità di un servizio di consegna rapido, efficiente e di qualità. Spinti dalla volontà di offrire ai nostri clienti la migliore esperienza culinaria direttamente a casa loro, abbiamo creato Deliveboo. La nostra missione è semplice: portare i vostri piatti preferiti direttamente alla vostra porta, mantenendo la freschezza e la qualità che desiderate e meritate.</p>
+</div>
+
 <div class="container d-flex">
     <div class="my-card">
         <div class="img-container">
@@ -27,7 +33,7 @@ export default {
         </div>
         <div class="text-container">
             <h1>Luca Tacchino</h1>
-            <p>mi sono divertito</p>
+            <p>Full Stack Web Developer</p>
         </div>
     </div>
     <div class="my-card">
@@ -35,8 +41,8 @@ export default {
             <img src="../../public/img/simpson/simpsons ale.jpg" alt="">
         </div>
         <div class="text-container">
-            <h1>Alessio</h1>
-            <p>mi sono divertito
+            <h1>Alessio Vinci</h1>
+            <p>Full Stack Web Developer
             </p>
         </div>
     </div>
@@ -45,8 +51,8 @@ export default {
             <img src="../../public/img/simpson/simpsons ema.jpg" alt="">
         </div>
         <div class="text-container">
-            <h1>Emanuele</h1>
-            <p>mi sono divertito</p>
+            <h1>Emanuele Verità</h1>
+            <p>Full Stack Web Developer</p>
         </div>
     </div>
     <div class="my-card">
@@ -55,7 +61,7 @@ export default {
         </div>
         <div class="text-container">
             <h1>Luca Membrini</h1>
-            <p>mi sono divertito</p>
+            <p>Full Stack Web Developer</p>
         </div>
     </div>
     <div class="my-card">
@@ -64,7 +70,7 @@ export default {
         </div>
         <div class="text-container">
             <h1>Samuele Conficconi</h1>
-            <p>mi sono divertito</p>
+            <p>Full Stack Web Developer</p>
         </div>
     </div>
 </div>
@@ -72,30 +78,76 @@ export default {
 </template>
 
 <style scoped lang="scss">
+
+p {
+    font-family: 'Montserrat', sans-serif !important;
+}
+
+.about-section {
+    text-align: center;
+    padding: 40px 20px; 
+    background-color: #f8f9fa; 
+    
+    h2 {
+        font-size: 2.5em; 
+        color: #343a40; 
+        margin-bottom: 20px; 
+        border-bottom: 3px solid #F8B932; 
+        display: inline-block; 
+        padding-bottom: 10px; 
+    }
+     
+    p {
+        font-size: 1.1em;
+        color: #6c757d;
+        line-height: 1.6;
+        max-width: 800px;
+        margin: 0 auto;   
+    }
+}
+
 .container{
-padding-top: 100px;
-display: flex;
-flex-wrap: wrap;
-justify-content: center;
+    padding-top: 100px;
+    display: flex;
+    flex-wrap: wrap; 
+    justify-content: center;
+
     .my-card{
-        width: calc(100% / 3);
+        flex: 1 1 calc(20% - 20px); 
+        max-width: calc(20% - 20px); 
+        margin: 0 10px;
+
         .img-container{
-          
+            position: relative;
+            width: 100%;
+            padding-bottom: 100%; // Mantiene un rapporto di aspetto 1:1
+            overflow: hidden; 
+
             img{
-                width: 400px;
-                height: 460px;
+                position: absolute; // Posiziona l'immagine rispetto al suo contenitore
+                top: 0;
+                left: 0;
+                width: 100%;
+                height: 100%;
                 object-fit: cover;
                 object-position: center;
-                
+                border-radius: 50%; // Rende l'immagine circolare
             }
         }
+        
         .text-container{
             text-align: center;
             width: 100%;
+            
             p{
                 word-break: break-all;
             }
         }
     }
 }
+
+</style>
+
+
+
 </style>
